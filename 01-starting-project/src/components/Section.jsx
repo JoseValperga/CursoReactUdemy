4caf4064105358417a20dec56fcb99554632e3d9
Jose Valperga <jose.valperga@gmail.com>
2025-02-14 11:42:19 -0300

    Antes de modificar Examples

--- 01-starting-project/src/components/Section.jsx
+++ 01-starting-project/src/components/Section.jsx
@@ -1,7 +1,7 @@
 const Section = ({ title, children, ...props }) => {
-  //...props le dice a JvaScrip que recolecte todas las otras props
+  //...props le dice a JavaScrip que recolecte todas las otras props
   //que puedan ser cebidas en este componente y los fusione
-  // en un objeto que podemos llamar "props". Cualquier nombre va bien
+  //en un objeto que podemos llamar "props". Cualquier nombre va bien
   //Es especialmente util para recibir configuraciones CSS. En este caso
   //estará recibiendo el id de <Section id="examples">
 
