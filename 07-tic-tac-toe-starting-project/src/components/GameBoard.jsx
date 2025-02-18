13f36a7015a859b667cf073ee3281b1a81b72ac3
Jose Valperga <jose.valperga@gmail.com>
2025-02-18 19:50:48 -0300

    Trabajando con el tablero

---
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      //la siguiente técnica se utiliza para actualizar
+      //estados que son arrays basados el estado anterior
+      // -> se crea una copia del array original
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      // -> y se utiliza esa copia
+      //para actualizar el estado
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  };
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
