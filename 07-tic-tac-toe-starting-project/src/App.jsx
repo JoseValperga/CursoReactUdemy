bd485189fb25d4a0b1409221b23bb005c8fc3597
Jose Valperga <jose.valperga@gmail.com>
2025-02-20 20:03:05 -0300

    Trabajandpo con estados

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,17 +1,67 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 const App = () => {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActiveState] = useState("X");
+
+  /*
+  const handleSelectSquare = () => {
+    setActiveState((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  };
+  se reemplaza abajo por una función de actualización de estado que asegura
+  que actualizemos el estado de una manera inmutable y que no estemos 
+  fusionando diferentes estados
+  */
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    //utilizamos abajo una forma de actualizar el estado actual con el estado anterior
+    setActiveState((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+
+    //Aquí abajo también actualizamos el estado actual utilizando el estado anterior
+    //que en este caso es un array. Este método está indicado por React para arrays
+    //NOTA: Las funciones de cambio de estado, en forma automática, pasan
+    //como parámetro el estado que tienen en ese momento cuando utilizamos
+    //una función flecha
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  };
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
       </div>
-      LOG
+      <Log />
     </main>
   );
 };
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-const GameBoard = () => {
+const GameBoard = ({ onSelectSquare, turns }) => {
+  /*
+  Ahora vamos a producir un estado derivado (o computado), gameBoard.
+  gameBoard se deriva de algún estado -> gameTurns que tenemos en App
+  y que hemos recibido una copia como prop llamada turns.
+  Es decir, a gameTurns (de App) lo transformamos en gameBoard
+  Y gameBoard no es otra cosa que el tablero de juego
+  En definitiva, en gameTurns tenemos el juego completo
+  y lo reconstruimos como gameBoard para renderizar
+  */
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  /*
+  Manejamos lo que sigue en App -> queda este comentario como referencia
+  
+  const GameBoard = ({ onSlectedSquare, activePlayerSimbol }) => {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
+
     setGameBoard((prevGameBoard) => {
       //la siguiente técnica se utiliza para actualizar
       //estados que son arrays basados el estado anterior
@@ -19,11 +39,13 @@ const GameBoard = () => {
       ];
       // -> y se utiliza esa copia
       //para actualizar el estado
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSimbol;
       return updatedBoard;
     });
-  };
 
+    onSlectedSquare()
+  };
+*/
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -31,7 +53,7 @@ const GameBoard = () => {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
