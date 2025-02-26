6fb6e1e68324bf666a9277e3cbffec10efbdfd48
Jose Valperga <jose.valperga@gmail.com>
2025-02-26 08:09:42 -0300

    continua...

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,10 +2,23 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+};
 
 const App = () => {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActiveState] = useState("X");
+
+  //const [activePlayer, setActiveState] = useState("X");
+  //Vamos a eliminar este estado y vamos a trabajar con gameTurns
 
   /*
   const handleSelectSquare = () => {
@@ -15,22 +28,24 @@ const App = () => {
   que actualizemos el estado de una manera inmutable y que no estemos 
   fusionando diferentes estados
   */
-
+  const activePlayer = deriveActivePlayer(gameTurns);
   const handleSelectSquare = (rowIndex, colIndex) => {
     //utilizamos abajo una forma de actualizar el estado actual con el estado anterior
-    setActiveState((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
-
+    //eliminamos la siguiente linea porque eliminarmos el estado activePlayer
+    //setActiveState((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    //y lo reemplazamos por un valor derivado
+for (const combination of WINNING_COMBINATIONS){
+  const firstSquareSymbol  
+  const secondSquareSymbol  
+  const thirdSquareSymbol
+}
     //Aquí abajo también actualizamos el estado actual utilizando el estado anterior
     //que en este caso es un array. Este método está indicado por React para arrays
     //NOTA: Las funciones de cambio de estado, en forma automática, pasan
     //como parámetro el estado que tienen en ese momento cuando utilizamos
     //una función flecha
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
