0cc765b6af818ca7bb07f4128b597a0685f734e3
Jose Valperga <jose.valperga@gmail.com>
2025-03-12 16:37:42 -0300

    Challenge 07 terminado

--- 07-challenge/src/App.jsx
+++ 07-challenge/src/App.jsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
-const INITIAL_USER_INPUT = {
-  initialInvestment: 1,
-  annualInvestment: 2,
-  expectedReturn: 3,
-  duration: 4,
-};
+const App = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
 
-function App() {
-  const [userInput, setUserInput] = useState(INITIAL_USER_INPUT);
+  const inputIsValid = userInput.duration >= 1;
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  };
+
   return (
     <main>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </main>
   );
-}
+};
 
 export default App;
